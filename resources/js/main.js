523a467618c667885d5cc6721fb343a54b351176
Kyle <Kyle@idea-PC.com>
2018-03-25 19:08:22 +0100

    again

--- resources/js/main.js
+++ resources/js/main.js
@@ -52,9 +52,6 @@ $('#find-smokey-button').on('click', function() {
   $('.dragonGallery').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'image'
-    gallery: {
-  enabled: true
-}
    });
 
 });
